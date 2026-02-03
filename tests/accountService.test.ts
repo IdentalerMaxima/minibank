71ea45bc5ad14afbee404dc00005bbb31188c8e6
Náray Koppány Titusz <koppany.naray@propairflight.com>
2026-02-03 13:35:19 +0100

    test(accountService): add deposit unit tests

--- tests/accountService.test.ts
+++ tests/accountService.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it} from "vitest";
-import { createAccount } from "../src/domain/accountService.ts";
+import { createAccount, deposit, DomainError } from "../src/domain/accountService.ts";
 
-describe("Account service", () => {
+describe("create account", () => {
     it("should create a normal acc with a welcome bonus", () => {
         const acc = createAccount("normal", "555-1111111-58", "Mr. Big Buck");
 
@@ -9,6 +9,23 @@ describe("Account service", () => {
         expect(acc.ownerName).toBe("Mr. Big Buck");             
         expect(acc.type).toBe("normal");                
         expect(acc.balance).toBe(10);                  
+    });
+});
+
+describe("deposit function", () => {
+    it("should increase the account balance for a normal deposit", () => {
+        const acc = createAccount("normal", "555-1111111-58", "Mr. Big Buck");
+        const updated = deposit(acc, 500);
+
+        expect(updated.balance).toBe(acc.balance + 500);
+        expect(acc.balance).toBe(10);
+    });
+
+    it("should throw DomainError for zero or negative deposit", () => {
+        const acc = createAccount("normal", "555-1111111-58", "Mr. Big Buck");
+
+        expect(() => deposit(acc, 0)).toThrow(DomainError);
+        expect(() => deposit(acc, -50)).toThrow(DomainError);
     });
 });
 
