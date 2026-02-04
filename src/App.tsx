7a2e6fa114bea13e12a6e944071b4c9320cf2e8e
Náray Koppány Titusz <koppany.naray@propairflight.com>
2026-02-04 09:08:44 +0100

    feat(pages): update app to include create acc page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { NavBar } from "./ui/components/NavBar";
+import { CreateAccountPage } from "./ui/pages/CreateAccountPage";
+import { appReducer } from "./app/reducer";
+import type { AppState } from "./app/state";
+
+const initialState: AppState = { accounts: [] };
 
 export function App() {
-  const [currentPage, setCurrentPage] = useState<
-    "list" | "create" | "deposit" | "withdraw" | "transfer"
-  >("list");
+    const [currentPage, setCurrentPage] = useState<"list" | "create" | "deposit" | "withdraw" | "transfer">("list");
 
-  return (
-    <div>
-      <h1>MiniBank</h1>
-      <NavBar setPage={setCurrentPage} />
-      <p>Current page: {currentPage}</p>
-    </div>
-  );
+    const [state, dispatch] = useReducer(appReducer, initialState);
+
+    return (
+        <div>
+            <h1>MiniBank</h1>
+            <NavBar setPage={setCurrentPage} />
+
+            {currentPage === "create" && <CreateAccountPage dispatch={dispatch} />}
+        </div>
+    );
 }
 
 export default App;
